2bb8fa70f49cdd8c0607601121ceaa8efeaa4987
saurabhraj25aug2004 <146987580+saurabhraj25aug2004@users.noreply.github.com>
2025-07-09 16:21:14 +0530

    frontend complete

---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,64 @@
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
+const PlaceOrder = () => {
+  const {getTotalCartAmount} =useContext(StoreContext)
+
+
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+
+        <div className="multi-fileds">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="street" />
+
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Sate" />
+        </div>
+
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+
+        <input type="text" placeholder="phone" />
+      </div>
+      {/* ---right----- */}
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+
+          <div>
+           <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0 ?0 :2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0 ?0: getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+
+          <button >
+            PROCEED TO PAYMENT
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default PlaceOrder;
